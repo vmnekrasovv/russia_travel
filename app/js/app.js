2ab5e0406458515a87c33a44cd2a01bfd2586ec4
vmnekrasovv <vmnekrasov777@gmail.com>
2025-01-19 23:49:33 +0300

    fix. section 2 post

--- app/js/app.js
+++ app/js/app.js
@@ -7,29 +7,6 @@ ScrollSmoother.create({
 (function($){
 	$(document).ready(function(){ 
 
-		$('.scroll-down').on('click', function(){
-			$('html, body').animate({
-				scrollTop: $('.navbar').offset().top
-			}, 500, function(){ 
-				//console.log('callback function'); 
-			});
-		});
-
-		$(window).on('scroll load', function(e){
-			if(pageYOffset > $('.header').outerHeight(true)){
-				$('.navbar').addClass('fixed');
-				$('body').addClass('navbar-fixed');
-			} else {
-				$('.navbar').removeClass('fixed');
-				$('body').removeClass('navbar-fixed');
-			}
-		});
-
-
-		// $('.header__image').parallax({
-		// 	imageSrc: 'images/dest/header.jpg',
-		// 	speed: .7,
-		// });
 
 		Fancybox.bind("[data-fancybox]", {
   		// Your custom options
